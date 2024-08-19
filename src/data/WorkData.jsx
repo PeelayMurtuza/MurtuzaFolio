b4cafb83c35e8b671184166424292be04a3a27c5
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-08-20 00:31:53 +0530

    completed PortFolio

---
+++ src/data/WorkData.jsx
@@ -0,0 +1,40 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Github Repo Visitor",
+        description:"It is build with  the React JS, TailwindCss and Github Api With searching and visiting functionalities. .",
+        tags:["react","TailwindCSS","GithubApi"],
+        demo:"github-repo-visitor.vercel.app",
+        github:"https://github.com/PeelayMurtuza/Github_Repo_Visitor"
+    },
+    {
+        id:2,
+        name:"Binary Search Visualizer",
+        description:"Visualizes binary search and Highlighting the mid-point, comparisons, and array partitioning",
+        tags:["HTML","CSS","JS"],
+        demo:"binary-search-visualization-six.vercel.app",
+        github:"https://github.com/PeelayMurtuza/Binary_Search_Visualization"
+        
+    },
+    {
+        id:3,
+        name:"Masjid Website",
+        description:"Real Time Masjid Website that Uses Reactjs,TailwindCss and LcomotiveJs for Smooth Scrollingv",
+        tags:["react","api","Inprogress"],
+        demo:"",
+        github:""
+    },
+    {
+        id:4,
+        name:"Portfolio Website ",
+        description:"A cool looking portfolio with  great user experience and build using react and framer motion",
+        tags:["React&Rectrouter","styledComponents",,"framer-motion"],
+        
+        demo:"",
+        github:""
+       
+    }  
+  
+] 
